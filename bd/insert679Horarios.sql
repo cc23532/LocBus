b301bd8d7cc9e587124b874173df5ab4cbe5d0c2
matheuscarletti03 <cc23532@g.unicamp.br>
2023-10-03 23:31:49 -0300

    Create insert679Horarios.sql

---
+++ bd/insert679Horarios.sql
@@ -0,0 +1,34 @@
+insert into locbus.Horários_Partida
+    (idLinha, sentido, horario)
+values
+
+--dias uteis
+    (679, 'Ida', 'Dia Útil', '04:50'),
+    (679, 'Ida', 'Dia Útil', '05:25'),
+    (679, 'Ida', 'Dia Útil', '05:45'),
+    (679, 'Ida', 'Dia Útil', '06:45'),
+    (679, 'Ida', 'Dia Útil', '07:45'),
+    (679, 'Ida', 'Dia Útil', '10:15'),
+    (679, 'Ida', 'Dia Útil', '12:15'),
+    (679, 'Ida', 'Dia Útil', '14:15'),
+    (679, 'Ida', 'Dia Útil', '15:45'),
+    (679, 'Ida', 'Dia Útil', '16:30'),
+    (679, 'Ida', 'Dia Útil', '16:45'),
+    (679, 'Ida', 'Dia Útil', '17:45'),
+    (679, 'Ida', 'Dia Útil', '18:45'),
+
+--sabados
+    (679, 'Ida', 'Sábados', '05:00'),
+    (679, 'Ida', 'Sábados', '06:40'),
+    (679, 'Ida', 'Sábados', '08:20'),
+    (679, 'Ida', 'Sábados', '11:00'),
+    (679, 'Ida', 'Sábados', '14:20'),
+    (679, 'Ida', 'Sábados', '17:40'),
+
+--domingos e feriados
+    (679, 'Ida', 'Domingos e Feriados', '05:00'),
+    (679, 'Ida', 'Domingos e Feriados', '06:40'),
+    (679, 'Ida', 'Domingos e Feriados', '08:20'),
+    (679, 'Ida', 'Domingos e Feriados', '11:00'),
+    (679, 'Ida', 'Domingos e Feriados', '14:20'),
+    (679, 'Ida', 'Domingos e Feriados', '17:40')
