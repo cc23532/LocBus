a3384233b059bc008033f8ef7f7c78ccfe0d5872
matheuscarletti03 <cc23532@g.unicamp.br>
2023-10-21 13:39:26 -0300

    Create insert_Linhas.sql

---
+++ bd/create_inserts/insert_Linhas.sql
@@ -0,0 +1,6 @@
+insert into locbus.Linhas
+    (idLinha, nomeIda, nomeVolta)
+values
+    (675, 'Campinas via Swift', 'Valinhos (Rodoviária)'),
+    (676, 'Campinas via Jd. do Lago', 'Valinhos (Jd. do Lago)'),
+    (677, 'Campinas (Rodoviária)', 'Valinhos (Pq. das Colinas)')
