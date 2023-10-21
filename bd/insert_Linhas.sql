fab893c6e483480b6618673d868f3446dfdb2903
matheuscarletti03 <cc23532@g.unicamp.br>
2023-10-21 13:40:09 -0300

    Delete insert_Linhas.sql

--- bd/insert_Linhas.sql
+++
@@ -1,6 +0,0 @@
-insert into locbus.Linhas
-    (idLinha, nomeIda, nomeVolta)
-values
-    (675, 'Campinas via Swift', 'Valinhos (Rodoviária)'),
-    (676, 'Campinas via Jd. do Lago', 'Valinhos (Jd. do Lago)'),
-    (677, 'Campinas (Rodoviária)', 'Valinhos (Pq. das Colinas)')
