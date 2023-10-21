39226aba2016e71b2068cdcbbdfb2da3c8c56b8d
matheuscarletti03 <cc23532@g.unicamp.br>
2023-10-21 13:39:17 -0300

    Create insert677_Horarios.sql

---
+++ bd/create_inserts/insert677_Horarios.sql
@@ -0,0 +1,59 @@
+insert into locbus.Horarios_Partida
+    (idLinha, sentido, dia, horario)
+values
+
+--ida
+--dias uteis
+    (677, 'Ida', 'Dia Útil', '04:50'),
+    (677, 'Ida', 'Dia Útil', '07:00'),
+    (677, 'Ida', 'Dia Útil', '09:20'),
+    (677, 'Ida', 'Dia Útil', '12:40'),
+    (677, 'Ida', 'Dia Útil', '15:00'),
+    (677, 'Ida', 'Dia Útil', '17:20'),
+    (677, 'Ida', 'Dia Útil', '19:40'),
+    
+--sabados
+    (677, 'Ida', 'Sábados', '04:50'),
+    (677, 'Ida', 'Sábados', '07:00'),
+    (677, 'Ida', 'Sábados', '09:20'),
+    (677, 'Ida', 'Sábados', '12:40'),
+    (677, 'Ida', 'Sábados', '15:00'),
+    (677, 'Ida', 'Sábados', '17:20'),
+    (677, 'Ida', 'Sábados', '19:40'),
+
+--domingos e feriados
+    (677, 'Ida', 'Domingos e Feriados', '04:50'),
+    (677, 'Ida', 'Domingos e Feriados', '07:00'),
+    (677, 'Ida', 'Domingos e Feriados', '09:20'),
+    (677, 'Ida', 'Domingos e Feriados', '12:40'),
+    (677, 'Ida', 'Domingos e Feriados', '15:00'),
+    (677, 'Ida', 'Domingos e Feriados', '17:20'),
+    (677, 'Ida', 'Domingos e Feriados', '19:40'),
+
+--volta
+--dias uteis
+    (677, 'Volta', 'Dia Útil', '05:50'),
+    (677, 'Volta', 'Dia Útil', '08:10'),
+    (677, 'Volta', 'Dia Útil', '11:30'),
+    (677, 'Volta', 'Dia Útil', '13:50'),
+    (677, 'Volta', 'Dia Útil', '16:10'),
+    (677, 'Volta', 'Dia Útil', '18:30'),
+    (677, 'Volta', 'Dia Útil', '21:50'),
+    
+--sabados
+    (677, 'Volta', 'Sábados', '05:50'),
+    (677, 'Volta', 'Sábados', '08:10'),
+    (677, 'Volta', 'Sábados', '11:30'),
+    (677, 'Volta', 'Sábados', '13:50'),
+    (677, 'Volta', 'Sábados', '16:10'),
+    (677, 'Volta', 'Sábados', '18:30'),
+    (677, 'Volta', 'Sábados', '21:50'),
+
+--domingos e feriados
+    (677, 'Volta', 'Domingos e Feriados', '05:50'),
+    (677, 'Volta', 'Domingos e Feriados', '08:10'),
+    (677, 'Volta', 'Domingos e Feriados', '11:30'),
+    (677, 'Volta', 'Domingos e Feriados', '13:50'),
+    (677, 'Volta', 'Domingos e Feriados', '16:10'),
+    (677, 'Volta', 'Domingos e Feriados', '18:30'),
+    (677, 'Volta', 'Domingos e Feriados', '21:50')
